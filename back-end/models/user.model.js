6e67fd5533fc069de6ac73aae6d47906fb5eb9ac
bravovo <bogdan.nyschyi@gmail.com>
2026-02-19 23:10:48 +0200

    remove editor role

--- back-end/models/user.model.js
+++ back-end/models/user.model.js
@@ -43,7 +43,7 @@ const userSchema = mongoose.Schema(
         },
         role: {
             type: String,
-            enum: ["civis", "editor", "admin"],
+            enum: ["civis", "admin"],
             default: "civis",
         },
     },
