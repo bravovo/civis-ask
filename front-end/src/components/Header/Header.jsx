e9b661695538a554ed54802f8c57fecf3cada410
bravovo <bogdan.nyschyi@gmail.com>
2026-05-22 23:51:06 +0300

    chore: add logo

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/icons/logo.png";
 
 function Header() {
   return (
     <header className="w-full bg-black flex justify-center sticky top-0 z-50 border-b border-b-gray-200 border-border">
-      <div className="max-w-7xl w-full flex items-center justify-between py-4 px-2 md:px-0 ">
+      <div className="max-w-7xl w-full flex items-center justify-between px-2 md:px-0 ">
         <div className="w-full">
-          <Link to="/" className="text-xl font-bold text-white tracking-wider">
-            Logo
+          <Link to="/">
+            <img src={logo} alt="Logo" className="h-20" />
           </Link>
         </div>
         <div className="w-full flex justify-end gap-4">
